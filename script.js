f6667506a102997ec73ab125a2135120522eecd4
Nevzudin Buzađija <nevzudinbuzadija@Nevzudins-MacBook-Air.local>
2025-02-11 20:55:26 +0100

    Main section completed and connection to server started

---
+++ script.js
@@ -0,0 +1,22 @@
+const createUser = () => {
+  fetch("https://c986-77-239-14-36.ngrok-free.app/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: "jasmin.fajkic@gmail.com",
+      password: "kalerias1982",
+      firstName: "Jasmin",
+      lastName: "Fajkic",
+    }),
+  })
+    .then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
